316144394110a28fedd8d9c9be8d8d63ffdaf950
Caue-155 <143454637+Caue-155@users.noreply.github.com>
2023-11-07 16:46:56 -0300

    Add files via upload

---
+++ questão 12.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    float soma;
+    int a;
+    float nota;
+    float media;
+
+    for (a = 1; a <= 3; a++)
+    {
+        do
+        {
+            printf("Digite a %dº nota:", a);
+            scanf("%f", &nota);
+
+            if (nota < 0 || nota > 10)
+            {
+                printf("Nota inválida. \nDigite a nota novamnete...\n\n");
+            }
+        } while (nota < 0 || nota > 10);
+
+        soma += nota;
+        media = soma / 3;
+    }
+
+    system("cls || clear");
+    printf("Média: %.2f \n\n", media);
+
+    return 0;
+}
